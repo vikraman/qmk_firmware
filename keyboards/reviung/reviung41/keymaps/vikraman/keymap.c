a8d8d9318e1a1d7948f5006fb51f301817b0084f
Vikraman Choudhury <git@vikraman.org>
2025-08-08 22:10:03 +0200

    add colemak layer

--- keyboards/reviung/reviung41/keymaps/vikraman/keymap.c
+++ keyboards/reviung/reviung41/keymaps/vikraman/keymap.c
@@ -16,53 +16,52 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE,
+    _QWERTY = 0,
+    _COLEMAK,
     _LOWER,
     _RAISE,
     _ADJUST
 };
 
+#define QWERTY DF(_QWERTY)
+#define COLEMAK DF(_COLEMAK)
+
 #define LOWER  TT(_LOWER)
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
 
-// Left-hand home row mods
-#define CTL_A LCTL_T(KC_A)
-#define ALT_S LALT_T(KC_S)
-#define GUI_D LGUI_T(KC_D)
-#define SFT_F LSFT_T(KC_F)
-
-// Right-hand home row mods
-#define SFT_J RSFT_T(KC_J)
-#define GUI_K RGUI_T(KC_K)
-#define ALT_L LALT_T(KC_L)
-#define CTL_SCLN RCTL_T(KC_SCLN)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT(
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    QK_GESC,  CTL_A,    ALT_S,    GUI_D,    SFT_F,     KC_G,               KC_H,     SFT_J,    GUI_K,    ALT_L,    CTL_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
-                                            KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_RGUI
+  [_QWERTY] = LAYOUT(
+    KC_TAB,           KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,            KC_BSPC,
+    QK_GESC,          LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,           KC_H,           RSFT_T(KC_J),   RGUI_T(KC_K),   LALT_T(KC_L),   RCTL_T(KC_SCLN), KC_QUOT,
+    KC_LSFT,          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,         RSFT_T(KC_ENT),
+                                                      KC_LALT,        LOWER,          KC_SPC,         RAISE,          KC_RGUI
   ),
-  
+
+  [_COLEMAK] = LAYOUT(
+    KC_TAB,           KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,         KC_BSPC,
+    QK_GESC,          LCTL_T(KC_A),   LALT_T(KC_R),   LGUI_T(KC_S),   LSFT_T(KC_T),   KC_G,           KC_M,           RSFT_T(KC_N),   RGUI_T(KC_E),   LALT_T(KC_I),   RCTL_T(KC_O),    KC_QUOT,
+    KC_LSFT,          KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_K,           KC_H,           KC_COMM,        KC_DOT,         KC_SLSH,         RSFT_T(KC_ENT),
+                                                      KC_LALT,        LOWER,          KC_SPC,         RAISE,          KC_RGUI
+  ),
+
   [_LOWER] = LAYOUT(
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
     _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
     _______,  KC_ESC,   KC_LGUI,  KC_LALT,  KC_CAPS,   KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  RSFT_T(KC_SPC),
                                             _______,   _______,  KC_ENT,   _______,  _______
   ),
-  
+
   [_RAISE] = LAYOUT(
     _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
     _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
     _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
                                             _______,   _______,  KC_BSPC,  _______,  _______
   ),
-  
+
   [_ADJUST] = LAYOUT(
-    UG_VALU,   UG_SATU, UG_HUEU,  UG_NEXT,  XXXXXXX,   UG_TOGG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    UG_VALD,   UG_SATD, UG_HUED,  UG_PREV,  XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    UG_VALU,   UG_SATU, UG_HUEU,  UG_NEXT,  QWERTY,    UG_TOGG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    UG_VALD,   UG_SATD, UG_HUED,  UG_PREV,  COLEMAK,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                             _______,   _______,  XXXXXXX,  _______,  _______
   ),

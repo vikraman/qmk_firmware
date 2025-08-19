523e083a89c78ca3e7c45245d46160774bf1b621
Vikraman Choudhury <git@vikraman.org>
2025-08-20 00:30:02 +0200

    port config to annepro2

--- keyboards/annepro2/keymaps/vikraman/keymap.c
+++ keyboards/annepro2/keymaps/vikraman/keymap.c
@@ -1,18 +1,18 @@
- /* Copyright 2021 OpenAnnePro community
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+/* Copyright 2021 OpenAnnePro community
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include QMK_KEYBOARD_H
 
@@ -21,6 +21,20 @@ enum anne_pro_layers {
     FN1,
     FN2,
 };
+
+#include "g/keymap_combo.h"
+
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define SFT_J RSFT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
 
 // clang-format off
 // Key symbols are based on QMK. Use them to remap your keyboard
@@ -52,11 +66,11 @@ enum anne_pro_layers {
 */
  const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [BASE] = LAYOUT_60_ansi( /* Base */
-    KC_ESC,           KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
-    KC_TAB,           KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-    KC_LCTL,          KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,          KC_QUOT,          KC_ENT,
-    KC_LSFT,                   KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
-    KC_LCTL,          KC_LALT, KC_LGUI,                   KC_SPC,             KC_RGUI, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
+    QK_GESC,          KC_1,    KC_2,    KC_3,  KC_4,  KC_5, KC_6,   KC_7,  KC_8,  KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,  KC_R,  KC_T, KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
+    LCTL_T(KC_ESC),   CTL_A,   ALT_S,   GUI_D, SFT_F, KC_G, KC_H,   SFT_J, GUI_K, ALT_L,   CTL_SCLN,         KC_QUOT,          KC_ENT,
+    KC_LSFT,                   KC_Z,    KC_X,  KC_C,  KC_V, KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
+    KC_LCTL,          KC_LALT, KC_LGUI,                   KC_SPC,                 KC_RGUI, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
 ),
   /*
   * Layer FN1
@@ -97,10 +111,33 @@ enum anne_pro_layers {
   */
  [FN2] = LAYOUT_60_ansi( /* FN2 */
     _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______, KC_AP_RGB_MOD, KC_AP_RGB_TOG, KC_AP_RGB_VAD, KC_AP_RGB_VAI, _______,
-    _______, _______,    KC_UP,      _______,    _______,    _______, _______, _______, _______, _______,       KC_PSCR,       KC_HOME,       KC_END,        _______,
-    _______, KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______, _______, _______, _______, _______,       KC_PGUP,       KC_PGDN,       _______,
-    _______,             _______,    _______,    _______,    _______, _______, _______, _______, _______,       KC_INS,        KC_DEL,        _______,
+    _______, _______,    KC_UP,      _______,    _______,    _______, EE_CLR,  _______, _______, _______,       KC_PSCR,       KC_HOME,       KC_END,        _______,
+    _______, KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______, QK_RBT,  _______, _______, _______,       KC_PGUP,       KC_PGDN,       _______,
+    _______,             _______,    _______,    _______,    _______, _______, QK_BOOT, _______, _______,       KC_INS,        KC_DEL,        _______,
     _______, _______,    _______,                                     _______,                   _______,       _______,       _______,       _______
  ),
 };
 // clang-format on
+
+void keyboard_post_init_user(void) {
+    ap2_led_enable();
+    ap2_led_set_profile(8);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case FN1:
+            // Set the leds to turquoise
+            ap2_led_set_foreground_color(0x77, 0xBB, 0x77);
+            break;
+        case FN2:
+            // Set the leds to green
+            ap2_led_set_foreground_color(0x77, 0xFF, 0x00);
+            break;
+        default:
+            // Reset back to the current profile
+            ap2_led_reset_foreground_color();
+            break;
+    }
+    return state;
+}

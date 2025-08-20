8d94cb04a84d25cd31ed49c9f7262ffa08f21ac4
Vikraman Choudhury <git@vikraman.org>
2025-08-20 23:54:45 +0200

    drop tap hold from tab

--- keyboards/annepro2/keymaps/vikraman/keymap.c
+++ keyboards/annepro2/keymaps/vikraman/keymap.c
@@ -68,7 +68,7 @@ enum anne_pro_layers {
  [BASE] = LAYOUT_60_ansi( /* Base */
     QK_GESC,          KC_1,    KC_2,    KC_3,  KC_4,  KC_5, KC_6,   KC_7,  KC_8,  KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
     KC_TAB,           KC_Q,    KC_W,    KC_E,  KC_R,  KC_T, KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-    LCTL_T(KC_ESC),   CTL_A,   ALT_S,   GUI_D, SFT_F, KC_G, KC_H,   SFT_J, GUI_K, ALT_L,   CTL_SCLN,         KC_QUOT,          KC_ENT,
+    KC_LCTL,          CTL_A,   ALT_S,   GUI_D, SFT_F, KC_G, KC_H,   SFT_J, GUI_K, ALT_L,   CTL_SCLN,         KC_QUOT,          KC_ENT,
     KC_LSFT,                   KC_Z,    KC_X,  KC_C,  KC_V, KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
     KC_LCTL,          KC_LALT, KC_LGUI,                   KC_SPC,                 KC_RGUI, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
 ),

073e66fc66496e4b30fd370ca53ab9a156998325
Vikraman Choudhury <git@vikraman.org>
2025-08-21 23:38:53 +0200

    get tap dances going

--- keyboards/annepro2/keymaps/vikraman/keymap.c
+++ keyboards/annepro2/keymaps/vikraman/keymap.c
@@ -22,7 +22,35 @@ enum anne_pro_layers {
     FN2,
 };
 
+// combos
 #include "g/keymap_combo.h"
+
+// tap dances
+enum { _TD_TAB_ESC = 0, _TD_BSPC_DELW, _TD_SPC_ENT };
+
+#define TAB_ESC TD(_TD_TAB_ESC)
+#define BSPC_DELW TD(_TD_BSPC_DELW)
+#define SPC_ENT TD(_TD_SPC_ENT)
+
+tap_dance_action_t tap_dance_actions[] = {
+    [_TD_TAB_ESC]   = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC),
+    [_TD_BSPC_DELW] = ACTION_TAP_DANCE_DOUBLE(KC_BSPC, LALT(KC_BSPC)),
+    [_TD_SPC_ENT]   = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_ENT),
+};
+
+// custom keys
+#define Q_CMD LT(0, KC_Q)
+#define W_CMD LT(0, KC_W)
+#define R_CMD LT(0, KC_R)
+#define T_CMD LT(0, KC_T)
+#define O_CMD LT(0, KC_O)
+#define LBRC_CMD LT(0, KC_LBRC)
+#define RBRC_CMD LT(0, KC_RBRC)
+#define Z_CMD LT(0, KC_Z)
+#define X_CMD LT(0, KC_X)
+#define C_CMD LT(0, KC_C)
+#define V_CMD LT(0, KC_V)
+#define N_CMD LT(0, KC_N)
 
 // Left-hand home row mods
 #define CTL_A LCTL_T(KC_A)
@@ -64,13 +92,13 @@ enum anne_pro_layers {
 * |       |       |       |                                 |       |  LEFT | DOWN  | RIGHT |
 * \-----------------------------------------------------------------------------------------/
 */
- const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [BASE] = LAYOUT_60_ansi( /* Base */
-    QK_GESC,          KC_1,    KC_2,    KC_3,  KC_4,  KC_5, KC_6,   KC_7,  KC_8,  KC_9,    KC_0,             KC_MINS,          KC_EQL,        KC_BSPC,
-    KC_TAB,           KC_Q,    KC_W,    KC_E,  KC_R,  KC_T, KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,             KC_LBRC,          KC_RBRC,       KC_BSLS,
-    KC_LCTL,          CTL_A,   ALT_S,   GUI_D, SFT_F, KC_G, KC_H,   SFT_J, GUI_K, ALT_L,   CTL_SCLN,         KC_QUOT,          KC_ENT,
-    KC_LSFT,                   KC_Z,    KC_X,  KC_C,  KC_V, KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
-    KC_LCTL,          KC_LALT, KC_LGUI,                   KC_SPC,                 KC_RGUI, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
+    QK_GESC,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,             KC_MINS,          KC_EQL,        BSPC_DELW,
+    TAB_ESC,  Q_CMD,   W_CMD,   KC_E,  R_CMD, T_CMD, KC_Y,   KC_U,  KC_I,  O_CMD,   KC_P,             LBRC_CMD,         RBRC_CMD,      KC_BSLS,
+    KC_LCTL,  CTL_A,   ALT_S,   GUI_D, SFT_F, KC_G,  KC_H,   SFT_J, GUI_K, ALT_L,   CTL_SCLN,         KC_QUOT,          KC_ENT,
+    KC_LSFT,           Z_CMD,   X_CMD, C_CMD, V_CMD, KC_B,   N_CMD, KC_M,  KC_COMM, KC_DOT,           KC_SLSH,          RSFT_T(KC_UP),
+    KC_LCTL,  KC_LALT, KC_LGUI,                   SPC_ENT,                 KC_RGUI, LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
 ),
   /*
   * Layer FN1
@@ -120,6 +148,8 @@ enum anne_pro_layers {
 // clang-format on
 
 void keyboard_post_init_user(void) {
+    autoshift_disable();
+
     ap2_led_set_manual_control(1);
     ap2_led_enable();
 
@@ -133,11 +163,17 @@ void keyboard_post_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case FN1:
-            ap2_led_t turquoise = {.p.red = 0x77, .p.green = 0xBB, .p.blue = 0x77, .p.alpha = 0xFF};
+            ap2_led_t turquoise = {.p.red   = 0x77,
+                                   .p.green = 0xBB,
+                                   .p.blue  = 0x77,
+                                   .p.alpha = 0xFF};
             ap2_led_blink(4, 10, turquoise, 5, 10);
             break;
         case FN2:
-            ap2_led_t green = {.p.red = 0x77, .p.green = 0xFF, .p.blue = 0x00, .p.alpha = 0xFF};
+            ap2_led_t green = {.p.red   = 0x77,
+                               .p.green = 0xFF,
+                               .p.blue  = 0x00,
+                               .p.alpha = 0xFF};
             ap2_led_blink(4, 11, green, 5, 10);
             break;
         default:
@@ -156,4 +192,42 @@ bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
         default:
             return false;
     }
+}
+
+bool send_cmd_on_hold(keyrecord_t *record, uint8_t keycode) {
+    if (!record->tap.count && record->event.pressed) {
+        tap_code16(G(keycode));
+        return false;
+    }
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case Q_CMD:
+            return send_cmd_on_hold(record, KC_Q);
+        case W_CMD:
+            return send_cmd_on_hold(record, KC_W);
+        case R_CMD:
+            return send_cmd_on_hold(record, KC_R);
+        case T_CMD:
+            return send_cmd_on_hold(record, KC_T);
+        case O_CMD:
+            return send_cmd_on_hold(record, KC_O);
+        case LBRC_CMD:
+            return send_cmd_on_hold(record, KC_LBRC);
+        case RBRC_CMD:
+            return send_cmd_on_hold(record, KC_RBRC);
+        case Z_CMD:
+            return send_cmd_on_hold(record, KC_Z);
+        case X_CMD:
+            return send_cmd_on_hold(record, KC_X);
+        case C_CMD:
+            return send_cmd_on_hold(record, KC_C);
+        case V_CMD:
+            return send_cmd_on_hold(record, KC_V);
+        case N_CMD:
+            return send_cmd_on_hold(record, KC_N);
+    }
+    return true;
 }
